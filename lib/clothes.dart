54d73645d35ab4703bf3d694b2b1eacd7c79805d
Andrey Cheban <77441445+LilAz7@users.noreply.github.com>
2024-10-19 19:12:32 +0300

    Update clothes.dart

--- lib/clothes.dart
+++ lib/clothes.dart
@@ -1,7 +1,11 @@
 class Clothing {
-  final String name;
-  final String imageUrl;
-  final double price;
+  final String name; // Название товара
+  final String imageUrl; // URL изображения
+  final double price; // Цена товара
 
-  Clothing({required this.name, required this.imageUrl, required this.price});
+  Clothing({
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+  });
 }
